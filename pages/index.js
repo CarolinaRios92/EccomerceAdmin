38d99cab259dbc17808fed40b74ed6e8312882a1
unknown <caritu_rios@hotmail.com>
2023-05-17 16:47:05 -0300

    crear productos

--- pages/index.js
+++ pages/index.js
@@ -3,11 +3,18 @@ import { useSession } from "next-auth/react";
 
 export default function Home() {
   const {data: session} = useSession();
-  if(!session) return;
   return (
     <Layout>
-      <div className="text-blue-900">
-        Hello, {session?.user?.email}
+      <div className="text-blue-900 flex justify-between">
+        <h2>
+          Hello, <b>{session?.user?.name}</b>
+        </h2>
+        <div className="flex bg-gray-200 text-black gap-1 rounded-lg overflow-hidden">
+          <img src={session?.user?.image} alt="image of user" className="w-6 h-6"/>
+          <span className="px-2">
+            {session?.user?.name}
+          </span>
+        </div>
       </div>
     </Layout>
   )
