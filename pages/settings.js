e0e95b5339e836f84dc95cd9b4521a0779ebb984
unknown <caritu_rios@hotmail.com>
2023-06-15 13:08:15 -0300

    settings

---
+++ pages/settings.js
@@ -0,0 +1,58 @@
+import Layout from "@/components/Layout";
+import Spinner from "@/components/Spinner";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+export default function SettingsPage(){
+    const [products, setProducts] = useState([]);
+    const [productsLoading, setProductsLoading] = useState(false);
+    const [featuredLoading, setFeaturedLoading] = useState(false);
+    const [featuredProductId, setFeaturedProductId] = useState("")
+
+    useEffect(() => {
+        setProductsLoading(true);
+        axios.get("/api/products").then(response => {
+            setProducts(response.data);
+            setProductsLoading(false);
+        });
+        setFeaturedLoading(true);
+        axios.get("/api/settings?name=featuredProductId").then(response => {
+            setFeaturedProductId(response.data.value)
+            setFeaturedLoading(false);
+        })
+    }, []);
+
+    async function saveSettings(){
+        await axios.put("/api/settings", {
+            name: "featuredProductId",
+            value: featuredProductId
+        })
+    }
+
+    return (
+        <Layout>
+            <h1>Configuración</h1>
+            {(productsLoading || featuredLoading) && (
+                <Spinner fullWidth={true}/>
+            )}
+            {(!productsLoading || !featuredLoading) && (
+                <>
+                    <label>Producto Destacado</label>
+                    <select value={featuredProductId} onChange={(e) => setFeaturedProductId(e.target.value)}>
+                        {products.length > 0 && products.map(product => (
+                            <option value={product._id}>{product.title}</option>
+                        ))}
+                    </select>
+                    <div>
+                        <button
+                            onClick={saveSettings} 
+                            className="btn-primary">
+                                Guardar Cambios
+                        </button>
+                    </div>
+                </>
+            )}
+            
+        </Layout>
+    )
+}
