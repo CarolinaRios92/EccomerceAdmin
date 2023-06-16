e4c89d4debf944d92cc909e967a6fc36875b6eeb
unknown <caritu_rios@hotmail.com>
2023-06-16 12:40:22 -0300

    dashboard

--- components/HomeHeader.js
+++ components/HomeHeader.js
@@ -7,13 +7,13 @@ export default function HomeHeader(){
             <h2 className="mt-0">
                 Hola, <b>{session?.user?.name}</b>
             </h2>
-            <div>
-            <div className="bg-gray-300 flex text-black gap-1 rounded-lg overflow-hidden">
-                <img src={session?.user?.image} alt="image of user" className="w-6 h-6"/>
-                <span className="px-2">
-                    {session?.user?.name}
-                </span>
-            </div>
+            <div className="hidden sm:block">
+                <div className="bg-gray-300 flex text-black gap-1 rounded-lg overflow-hidden">
+                    <img src={session?.user?.image} alt="image of user" className="w-6 h-6"/>
+                    <span className="px-2">
+                        {session?.user?.name}
+                    </span>
+                </div>
             </div>
         </div>
     )
--- components/HomeStats.js
+++ components/HomeStats.js
@@ -1,53 +1,93 @@
+import { useState, useEffect } from "react";
+import Spinner from "./Spinner";
+import axios from "axios";
+import subHours from "date-fns/subHours";
+
 export default function HomeStats(){
+    const [orders, setOrders] = useState([]);
+    const[isLoading, setIsLoading] = useState(false);
+
+    useEffect(() => {
+        setIsLoading(true);
+        axios.get("api/orders").then(res => {
+            setOrders(res.data);
+            setIsLoading(false);
+        })
+    },[]);
+
+    if(isLoading){
+        return (
+            <div className="my-20">
+                <Spinner fullWidth={true} />
+            </div>
+        )
+    }
+
+    const ordersToday = orders.filter(o => new Date(o.createdAt) > subHours(new Date, 24));
+    const ordersWeek = orders.filter(o => new Date(o.createdAt) > subHours(new Date, 24*7));
+    const ordersMonth = orders.filter(o => new Date(o.createdAt) > subHours(new Date, 24*30));
+    
+    function ordersTotal(orders){
+        let sum = 0;
+        orders.forEach(order => {
+            const {line_items} = order;
+            line_items.forEach(li => {
+                const lineSum = li.quantity * li.unit_price;
+                sum += lineSum;
+            });
+        });
+        return sum;
+    } 
+
     return(
         <div>
             <h2>Ordenes</h2>
-            <div className="grid grid-cols-3 gap-4">
+            <div className="tiles-grid">
                 <div className="tile">
                     <h3 className="tile-header">
                         Hoy
                     </h3>
-                    <div className="tile-number">2</div>
-                    <div className="title-desc">2 ordenes hoy</div>
+                    <div className="tile-number">{ordersToday.length}</div>
+                    <div className="title-desc">{ordersToday.length} ordenes hoy</div>
                 </div>
                 <div className="tile">
                     <h3 className="tile-header">
                         Esta Semana
                     </h3>
-                    <div className="tile-number">2</div>
-                    <div className="title-desc">2 ordenes hoy</div>
+                    <div className="tile-number">{ordersWeek.length}</div>
+                    <div className="title-desc">{ordersWeek.length} ordenes esta semana</div>
                 </div>
                 <div className="tile">
                     <h3 className="tile-header">
                         Este Mes
                     </h3>
-                    <div className="tile-number">2</div>
-                    <div className="title-desc">2 ordenes hoy</div>
+                    <div className="tile-number">{ordersMonth.length}</div>
+                    <div className="title-desc">{ordersMonth.length} ordenes este Mes</div>
                 </div>
             </div>
 
             <h2>Ganacias</h2>
-            <div className="grid grid-cols-3 gap-4">
+            <div className="tiles-grid">
                 <div className="tile">
                     <h3 className="tile-header">
                         Hoy
                     </h3>
-                    <div className="tile-number">$20</div>
-                    <div className="title-desc">2 ordenes hoy</div>
+                    <div className="tile-number">${ordersTotal(ordersToday)}</div>
+                    <div className="title-desc">{ordersToday.length} ordenes hoy</div>
                 </div>
                 <div className="tile">
                     <h3 className="tile-header">
                         Esta Semana
                     </h3>
-                    <div className="tile-number">$122</div>
-                    <div className="title-desc">2 ordenes hoy</div>
+                    <div className="tile-number">${ordersTotal(ordersWeek)}</div>
+                    <div className="title-desc">{ordersWeek.length} ordenes esta semana</div>
                 </div>
                 <div className="tile">
                     <h3 className="tile-header">
                         Este Mes
                     </h3>
-                    <div className="tile-number">$2550</div>
-                    <div className="title-desc">2 ordenes hoy</div>
+                    <div className="tile-number">${ordersTotal(ordersMonth)}</div>
+                    <div className="title-desc">{ordersMonth.length} ordenes este Mes</div>
                 </div>
             </div>
         </div>
